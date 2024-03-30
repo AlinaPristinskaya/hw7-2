6b145fb6e17ab8ed0fb146fe101cb6517f982625
Alina Prystynska <pristinskayaalina9@gmail.com>
2024-03-30 17:15:12 +0000

    30/03

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,16 @@
 function checkPalindrome() {
-    // Get the input value
+    
     let word = document.getElementById("wordInput").value.toLowerCase();
 
-    // Remove spaces from the word
+   
     let cleanedWord = word.replace(/\s/g, '');
 
-    // Initialize variables for looping
+   
     let left = 0;
     let right = cleanedWord.length - 1;
     let isPalindrome = true;
 
-    // Check if the word is a palindrome using a while loop
+   
     while (left < right) {
         if (cleanedWord[left] !== cleanedWord[right]) {
             isPalindrome = false;
@@ -20,7 +20,7 @@ function checkPalindrome() {
         right--;
     }
 
-    // Print the result
+    
     if (isPalindrome) {
         console.log(word + " is a palindrome.");
     } else {
